2518229442a6a99acb27c2385797c23f847313d2
S10257932H <S10257932@connect.np.edu.sg>
2023-07-25 11:13:54 +0800

    yo

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -2,12 +2,15 @@ from pathlib import Path
 import csv
 
 # create a file to csv file.
-fp = Path.cwd() / 'PFB_grp_proj' / 'project_group' / 'cash on hand.csv'
+fp = Path.cwd() / 'csv_reports' / 'cash on hand.csv'
 
 # read the csv file to append from the csv.
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
     next(reader) 
 
-# create empty list to store cash on hand records
-cash_on_hand = []
+    # create empty list to store cash on hand records
+    cash_on_hand = []
+
+    # append cash on hand records into the cash_on_hand list
+    for row in reader:
