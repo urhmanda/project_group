9d79726aae95c7c78b24f50d7485d3b1ddae189a
S10257932H <S10257932@connect.np.edu.sg>
2023-07-25 11:07:43 +0800

    ok

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -4,3 +4,5 @@ fp = Path.cwd() / 'PFB_grp_proj' / 'cash on hand.csv'
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
     next(reader) 
+
+cash_on_hand = 
