a555d9942baf6f1302036bd4d11c80d99962b5e4
Asteract <remus.choy@hotmail.com>
2023-07-25 11:22:16 +0800

    ye

--- profit_and_loss.py
+++ profit_and_loss.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import csv
 
 fp = Path.cwd()/"profit and loss.csv"
+
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
     next(reader)
