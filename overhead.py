ad94cc45ba258661416e002bfa20ce27e5cb4469
urhmanda <s10258292@connect.np.edu.sg>
2023-07-25 11:14:59 +0800

    added _ to csv file name

--- overhead.py
+++ overhead.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 import csv
 
-fp = Path.cwd()/"overhead.csv"
+fp = Path.cwd()/"csv_reports"/"overhead.csv"
 
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
     next(reader) 
+
