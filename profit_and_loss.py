766c14cd706e99cbb1782c15df0d5a31cd5693bb
Asteract <remus.choy@hotmail.com>
2023-07-25 11:28:37 +0800

    profit loss

--- profit_and_loss.py
+++ profit_and_loss.py
@@ -5,4 +5,8 @@ fp = Path.cwd()/"profit and loss.csv"
 
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
-    next(reader)
+    next(reader) #To skip the header
+
+#Creates an empty list to store profit and loss records
+profit_and_loss=[]
+
