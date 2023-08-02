c161b3b6e4672c8f0b4c8bc9a195a1acb86ab624
urhmanda <s10258292@connect.np.edu.sg>
2023-08-02 11:37:49 +0800

    changed csb file name

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import cash_on_hand, overhead, profit_and_loss
 
-overhead.inside_overhead()
-cash_on_hand.inside_COH()
-profit_and_loss.inside_PNL()
-
+def main():
+    overhead.inside_overhead()
+    cash_on_hand.inside_COH()
+    profit_and_loss.inside_PNL()
+main()
 
 
