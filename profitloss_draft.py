a88ae3f473978cb8b8609c23f06d6cd5283a4a07
urhmanda <s10258292@connect.np.edu.sg>
2023-08-03 01:03:16 +0800

    .

--- profitloss_draft.py
+++ profitloss_draft.py
@@ -10,6 +10,9 @@ def profit_loss_function():
             day, profit = data[i]
             prev_day, prev_profit = data[i - 1]
             if profit > prev_profit:
+                """
+                - To calculate difference in amount of profit if current day's profit is lesser than previous day's
+                """                
                 profit_deficit = profit - prev_profit
                 profit_deficit_list.append((day, profit_deficit))
         return profit_deficit_list
@@ -19,23 +22,25 @@ def profit_loss_function():
         - This function reads data from csv file and return a list of tuples
         '''
         data = []
-        # Provide the relative file path directly
         with open(file_path, newline='') as csvfile:
             reader = csv.reader(csvfile)
             next(reader)  # Skip the header if it exists
             for row in reader:
+                """
+                - To add data for number of days and amount of profit into "data", which is an empty list
+                """                
                 day = int(row[0])
                 profit = int(row[1])
                 data.append((day, profit))
         return data
 
-    # Provide the relative file path directly for 'PNL.csv'
+    # Provide the relative file path directly 
     csv_file_path = "csv_reports/profit_loss.csv"
     data = read_csv_data(csv_file_path)
 
     profit_deficits = find_profit_deficit(data)
 
-    result_str = "" # result_str will store formatted output as a string
+    result_str = "" # result_str will store formatted profit deficits information as a string
     for day, deficit in profit_deficits:
         result_str += f"[PROFIT DEFICIT] Day: {day}, Amount: USD{deficit}\n"
     return result_str
