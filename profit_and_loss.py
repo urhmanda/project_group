671359fa1195e37b216959ae4ad6dc91c83aa45e
urhmanda <s10258292@connect.np.edu.sg>
2023-08-03 00:52:08 +0800

    ..

--- profit_and_loss.py
+++ profit_and_loss.py
@@ -19,23 +19,23 @@ def profit_loss_function():
         return profit_deficit_list
 
     def read_csv_data(file_path):
-            """
-            - This function reads data from csv file and return a list of tuples
-            """
-            data = []
-            with open(file_path, newline='') as csvfile:
-                reader = csv.reader(csvfile)
-                next(reader)  # Skip the header if it exists
-                for row in reader:
-                    """
-                    - To add data for number of days and amount of profit into "data", which is an empty list
-                    """
-                    day = int(row[0])
-                    profit = int(row[1])
-                    data.append((day, profit))
-            return data
+        """
+        - This function reads data from csv file and return a list of tuples
+        """
+        data = []
+        with open(file_path, newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            next(reader)  # Skip the header if it exists
+            for row in reader:
+                """
+                - To add data for number of days and amount of profit into "data", which is an empty list
+                """
+                day = int(row[0])
+                profit = int(row[1])
+                data.append((day, profit))
+        return data
 
-        # Provide the relative file path directly
+    # Provide the relative file path directly
     csv_file_path = "csv_reports/profit_loss.csv"
     data = read_csv_data(csv_file_path)
 
