75cc9926748aa53ca80765a92709ab42222ff8ed
urhmanda <s10258292@connect.np.edu.sg>
2023-08-02 10:46:38 +0800

    .

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cash_on_hand, overhead, profit_and_loss
 
 cash_on_hand.inside_cash()
 overhead.inside_overhead()
-profit_and_loss.inside_profitnloss()
+# profit_and_loss.inside_profitnloss()
 
 
 
